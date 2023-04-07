72ef35b2fda027c9b5a52431ea6cf740043918d0
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-04-07 18:19:25 -0400

    implement Head in app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,19 @@
 import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google'
+import Head from 'next/head'
 
 const monterrat = Montserrat({ subsets: ['latin'], variable: '--font-mont' })
 
 export default function App ({ Component, pageProps }) {
   return (
-    <main className={`${monterrat.variable} font-mont`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={`${monterrat.variable} font-mont`}>
+        <Component {...pageProps} />
+      </main>
+    </>
   )
 }
