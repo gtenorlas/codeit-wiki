2826714a4a8c6f8d225765e9478577b31c150726
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-04-07 20:20:01 -0400

    add colors to tailwind.config.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,7 +11,7 @@ export default function App ({ Component, pageProps }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className={`${monterrat.variable} font-mont`}>
+      <main className={`${monterrat.variable} font-mont bg-light w-full min-h-screen`}>
         <Component {...pageProps} />
       </main>
     </>
