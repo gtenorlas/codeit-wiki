1de3c522b874ba386ccada5cb82b096f4dcf4c84
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-04-08 18:07:34 -0400

    implement CustomLink in the Navbar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,30 @@
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
+
+const CustomLink = ({ href, title, className }) => {
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span className="h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300">
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/blogs">Blogs</Link>
-        <Link href="/">Hire me</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/blogs" title="Blogs" className="mx-4" />
+        <CustomLink href="/" title="Hire Me" className="ml-4" />
       </nav>
-      <h2>Logo</h2>
+
       <nav>
         <Link href="/" target={"_blank"}>
           T
@@ -32,6 +45,9 @@ const NavBar = () => {
           T
         </Link>
       </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 };
