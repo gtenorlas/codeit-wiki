1591766aaa217de98ade27f5f3d149df61345735
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-04-10 23:27:12 -0400

    implement LinkArrow in components/Icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,38 @@ import Layout from '@/components/Layout'
 import Image from 'next/image'
 import profilePic from '../../public/images/profile/developer-pic-gpt.png'
 import AnimatedText from '@/components/AnimatedText'
+import Link from 'next/link'
 
 export default function Home () {
   return (
     <main className='flex items-center text-dark w-full min-h-screen'>
-      <Layout className="pt-0">
+      <Layout className='pt-0'>
         <div className='flex items-center justify-between w-full'>
           <div className='w-1/2'>
-            <Image src={profilePic} alt="CodeIt.Wiki" className="w-full h-auto"></Image>
+            <Image
+              src={profilePic}
+              alt='CodeIt.Wiki'
+              className='w-full h-auto'
+            ></Image>
           </div>
           <div className='w-1/2 flex flex-col items-center self-center'>
-            <AnimatedText text="Turning Vision Into Reality With Code and Design." className='!text-6xl !text-left'/>
-            <p>As a skilled developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and blogs, showcasing my experticse in React.js and web development.</p>
+            <AnimatedText
+              text='Helping provide solutions through Code and Design.'
+              className='!text-5xl !text-left'
+            />
+            <p className='my-4 text-base'>
+            As a highly skilled Full-Stack Developer, I am committed to staying at the forefront of technology trends and constantly enhancing my skill set. With a keen eye for innovation, I am actively seeking new opportunities to leverage my versatile skills and add value to a dynamic team. If you are in need of a dependable and personable Full-Stack Developer who consistently delivers exceptional results, look no further. Let's collaborate to create cutting-edge web applications that propel your business to new heights of success.
+            </p>
+            <div className='flex item-center self-start mt-2'>
+              <Link href='dummy.pdf' target='_blank'
+              className="flex item-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
+              >
+                Resume
+              </Link>
+              <Link href='mailto:gene.tenorlas@yahoo.com' target='_blank'>
+                Contact Me
+              </Link>
+            </div>
           </div>
         </div>
       </Layout>
