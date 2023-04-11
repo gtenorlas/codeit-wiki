0a23b845e4879b8a21971dcc3239b1dacf140b68
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-04-11 16:50:48 -0400

    create link to resume and contact me

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import Layout from '@/components/Layout'
 import Image from 'next/image'
-import profilePic from '../../public/images/profile/developer-pic-gpt.png'
+import profilePic from '../../public/images/profile/developer-pic-gpt3.png'
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
+import { LinkArrow } from '@/components/Icon'
 
 export default function Home () {
   return (
@@ -24,13 +25,15 @@ export default function Home () {
             <p className='my-4 text-base'>
             As a highly skilled Full-Stack Developer, I am committed to staying at the forefront of technology trends and constantly enhancing my skill set. With a keen eye for innovation, I am actively seeking new opportunities to leverage my versatile skills and add value to a dynamic team. If you are in need of a dependable and personable Full-Stack Developer who consistently delivers exceptional results, look no further. Let's collaborate to create cutting-edge web applications that propel your business to new heights of success.
             </p>
-            <div className='flex item-center self-start mt-2'>
+            <div className='flex items-center self-start mt-2'>
               <Link href='dummy.pdf' target='_blank'
               className="flex item-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
               >
-                Resume
+                Resume <LinkArrow className="w-6 ml-1"/>
               </Link>
-              <Link href='mailto:gene.tenorlas@yahoo.com' target='_blank'>
+              <Link href='mailto:gene.tenorlas@yahoo.com' target='_blank'
+              className="ml-4 text-lg font-medium capitalize text-dark underline"
+              >
                 Contact Me
               </Link>
             </div>
