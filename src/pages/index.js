6a70b767fc21af3a667c79864c9ce6ae61a91cf3
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-04-13 23:48:46 -0400

    Format css to Circular

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import profilePic from '../../public/images/profile/developer-pic-gpt1.png'
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icon'
+import HireMe from '@/components/HireMe'
 
 export default function Home () {
   return (
@@ -40,6 +41,7 @@ export default function Home () {
           </div>
         </div>
       </Layout>
+      <HireMe/>
     </main>
   )
 }
