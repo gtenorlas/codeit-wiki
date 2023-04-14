33e4b32c05adf6cb87e479535b49ff2f01e0b568
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-04-13 22:14:56 -0400

    implement footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+      <Layout className="py-8 flex flex-center justify-between">
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+        <div className="flex items-center">Developed by <span>Gene Tenorlas</span>from CodeIt.Wiki</div>
+        <Link href="mailto:gene.tenorlas@yahoo.com" target="_blank">Contact Me</Link>
+      </Layout>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
