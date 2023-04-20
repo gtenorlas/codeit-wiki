64207553396e448f03cb030cf3e03689bf18e455
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-04-20 13:46:57 -0400

    add AnimatedText in about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,18 @@
+import AnimatedText from '@/components/AnimatedText'
 import Head from 'next/head'
 import React from 'react'
 
 const about = () => {
   return (
     <>
-    <Head>
-      <title>Code It .Wiki | About Page</title>
-      <meta name="keywords" content="about" />
-      <meta name="description" content="About Page" />
-    </Head>
+      <Head>
+        <title>Code It .Wiki | About Page</title>
+        <meta name='keywords' content='about' />
+        <meta name='description' content='About Page' />
+        <main>
+          <AnimatedText text='I Help Solve Problems Through Code' />
+        </main>
+      </Head>
     </>
   )
 }
