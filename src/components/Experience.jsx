f2947f13c5e835e0693afc70470edc0cc6a43fdc
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-04-29 19:55:59 -0400

    set css for li in Details

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
-    <li>
+    <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
       <div>
         <h3>
           {position}&nbsp;<a href={companyLink}>@{company}</a>
@@ -25,7 +25,12 @@ const Experience = () => {
 
       <div className="w-[75%] mx-auto relative">
         <ul>
-          <Details />
+          <Details
+          position="Back-End Developer" company="RB&W"
+          time="January 2020 - April 2020"
+          address="Brampton, ON"
+          work="Developed API using Java and Spring. Test API using Postman, curl, and Junit"
+          />
           </ul>
       </div>
     </div>
