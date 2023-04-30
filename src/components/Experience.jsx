0d0a9bbcbb416d3b23967e170aa3be7aec80c5d0
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-04-29 20:02:12 -0400

    set class for span in Details

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,13 +4,13 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
     <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
       <div>
-        <h3>
-          {position}&nbsp;<a href={companyLink}>@{company}</a>
+        <h3 className="capitalize font-bold text-2xl">
+          {position}&nbsp;<a href={companyLink} target="_blank" className="text-primary capitalize">@{company}</a>
         </h3>
-        <span>
+        <span className="capitalize font-medium text-dark/75">
           {time} | {address}
         </span>
-        <p>{work}</p>
+        <p className="font-medium w-full">{work}</p>
       </div>
     </li>
   );
