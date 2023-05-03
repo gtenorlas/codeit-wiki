07dd43e35cc0c1f3328592ff147f20a7d1c64a9b
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-03 10:21:29 -0400

    implement LiIcon and add animation to circles

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useScroll, motion } from "framer-motion";
 import { useRef } from "react";
+import LiICon from "./LiICon";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
+ const ref=useRef(null);
   return (
-    <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+     <LiICon reference={ref}/>
       <div>
         <h3 className="capitalize font-bold text-2xl">
           {position}&nbsp;
@@ -40,7 +43,7 @@ const Experience = () => {
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"
         />
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
--- src/components/LiICon.jsx
+++ src/components/LiICon.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { motion,useScroll } from 'framer-motion'
 
-const LiICon = () => {
+
+const LiICon = ({reference}) => {
+  const {scrollYProgress} = useScroll(
+    {
+      target: reference,
+      offset: ['center end', 'center center']
+    }
+  )
   return (
-    <div>LiICon</div>
+    <figure className='absolute left-0 stroke-dark'>
+      <svg className='-rotate-90' width="75" height="75" viewBox='0 0 100 100'>
+        <circle cx="75" cy='50' r='20' className='stroke-primary stroke-1 fill-none'/>
+        <motion.circle cx="75" cy='50' r='20' className='fill-light stroke-[5px]'
+        style={{
+          pathLength:scrollYProgress
+        }}
+        />
+        <circle cx="75" cy='50' r='10' className='fill-primary stroke-1 animate-pulse'/>
+      </svg>
+    </figure>
   )
 }
 
