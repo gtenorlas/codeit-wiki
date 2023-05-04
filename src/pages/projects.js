cd141475acfd2d8cc650944d2c7fd68e92f72649
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-04 19:01:55 -0400

    implement Project component in projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -45,6 +45,43 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   )
 }
 
+const Project = ({ title, type, img, link, github }) => {
+  return (
+    <article>
+      <Link
+        href={link}
+        target='_blank'
+        className='w-full cursor-pointer overflow-hidden rounded-lg'
+      >
+        <Image src={img} alt={title} className='w-full auto' />
+      </Link>
+      <div className='w-full flex flex-col items-start justify-between pl-6'>
+        <span className='text-primary font-medium text-xl'>{type}</span>
+        <Link
+          href={link}
+          target='_blank'
+          className='hover:underline underline-offset-2'
+        >
+          <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
+        </Link>
+        <div className='mt-2 flex items-center'>
+          <Link href={github} target='_blank' className='w-10'>
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target='_blank'
+            className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
+          '
+          >
+            Visit
+          </Link>
+        </div>
+      </div>
+    </article>
+  )
+}
+
 const projects = () => {
   return (
     <>
@@ -56,7 +93,7 @@ const projects = () => {
 
       <main className='w-full mb-16 fle flex-col items-center justify-center'>
         <Layout className='pt-16'>
-          <AnimatedText text='Solving is part of learning' className='mb-16'/>
+          <AnimatedText text='Solving is part of learning' className='mb-16' />
 
           <div className='grid grid-cols-12 gap-24'>
             <div className='col-span-12'>
@@ -69,7 +106,16 @@ const projects = () => {
                 summary='This is my portfolio website that I created using Next.js, Tailwind CSS, and Framer Motion.'
               />
             </div>
-            <div className='col-span-6'>Project-1</div>
+            <div className='col-span-6'>
+              <Project
+                type='Project'
+                title='Quiz App'
+                img='quiz'
+                github='/'
+                link='/'
+
+              />
+            </div>
             <div className='col-span-6'>Project-2</div>
             <div className='col-span-12'>Featured Project</div>
             <div className='col-span-6'>Project-3</div>
