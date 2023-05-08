af2b404d76b170799fe8a1e8be7daa99846e497d
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-08 15:02:54 -0400

    implement useEffect in useTheSwitcher to check local storage or media query

--- src/components/hooks/useThemeSwitcher.jsx
+++ src/components/hooks/useThemeSwitcher.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const useThemeSwitcher = () => {
+  const prefersDarkMode = "(prefers-color-scheme: dark)"; // media query
+  const [mode, setMode] = useState("");
+
+  //check local storage or media query for theme
+  useEffect(() => {
+    const mediaQuery=window.matchMedia(prefersDarkMode)
+    const userPref= window.localStorage.getItem("theme");
+
+    const handleChange = () => {
+      if(userPref){
+        let check=userPref === "dark" ? true : false;
+        setMode(check);
+        if(check==="dark") {
+          document.documentElement.classList.add("dark");
+        }else{
+          document.documentElement.classList.remove("dark");
+        }
+      }else {
+        let check=mediaQuery.matches ? true : false;
+        setMode(check);
+        if(check==="dark") {
+          document.documentElement.classList.add("dark");
+        }else{
+          document.documentElement.classList.remove("dark");
+        }
+      }
+    }
+    mediaQuery.addEventListener("change", handleChange)
+
+    return()=>mediaQuery.removeEventListener("change", handleChange)
+  }, [])
+
   return (
     <div>useThemeSwitcher</div>
   )
