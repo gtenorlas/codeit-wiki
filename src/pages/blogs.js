e2b8ae7266a068e077c126a54176c35c57acf103
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-08 14:26:51 -0400

    create animation in Blog motion.li

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -38,7 +38,7 @@ const MovingImg = ({ title, img, link }) => {
       </h2>
       <FramerImage
       style={{x,y}}
-      initial={{opacity:0}}
+      initial={{opancity:0}}
       whileInView={{opacity:1,transition:{duration:0.2}}}
       ref={imgRef}
         src={img}
@@ -51,14 +51,17 @@ const MovingImg = ({ title, img, link }) => {
 
 const Blog = ({ img, title, date, link }) => {
   return (
-    <li
+    <motion.li
+    initial={{y:200}}
+    whileInView={{y:0, transition:{duration:0.5, ease:"easeInOut"}}}
+    viewport={{once:true}}
       className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark
     border-r-4 border-b-4
     '
     >
       <MovingImg title={title} img={img} link={link} />
       <span className='text-primary font-semibold pl-4'>{date}</span>
-    </li>
+    </motion.li>
   )
 }
 
