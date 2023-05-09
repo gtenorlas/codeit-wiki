4bae3194546f99db07800f7f4b335b701b1cfd39
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-09 15:32:11 -0400

    add animation to the hamburger button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import { useRouter } from "next/router";
 import {
@@ -47,10 +47,25 @@ const NavBar = () => {
       className="w-full px-32 py-8 font-medium flex items-center justify-between
     dark:text-light"
     >
-      <button className="flex flex-col justify-center items-center" onClick={handleClick}>
-        <span className="bg-dark dark:bg-light block h-0.5 w-6 rounded-sm -translate-y-0.5"></span>
-        <span className="bg-dark dark:bg-light block h-0.5 w-6 rounded-sm my-0.5"></span>
-        <span className="bg-dark dark:bg-light block h-0.5 w-6 rounded-sm translate-y-0.5"></span>
+      <button
+        className="flex flex-col justify-center items-center"
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm -translate-y-0.5
+        ${isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"}
+        `}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5
+        ${isOpen ? "opacity-0" : "opacity-100"}
+        `}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm -translate-y-0.5
+        ${isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"}
+        `}
+        ></span>
       </button>
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
