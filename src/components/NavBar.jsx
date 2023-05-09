8e4b457fa986bf97d866856b89ebf10284c92b88
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-09 18:15:40 -0400

    add animation to mobile navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -167,7 +167,8 @@ const NavBar = () => {
       </div>
       {isOpen ? (
         <motion.div
-
+initial={{scale:0,opacity:0,x:"-50%",y:"-50%"}}
+animate={{scale:1,opacity:1}}
 
           className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
       bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32
