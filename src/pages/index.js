6184318f415d9c0f3182957c9ff845d0161ddff1
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-09 19:40:41 -0400

    change HireMe component position for medium screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,19 @@ import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 export default function Home () {
   return (
     <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-      <Layout className='pt-0'>
-        <div className='flex items-center justify-between w-full'>
-          <div className='w-1/2 pr-8'>
+      <Layout className='pt-0 md:pt-8 sm:pt-8'>
+        <div className='flex items-center justify-between w-full lg:flex-col'>
+          <div className='w-1/2 pr-8 md:w-full md:pr-0 md:h-auto'>
             <Image
               src={profilePic}
               alt='CodeIt.Wiki'
-              className='w-full h-auto pr-8'
+              className='w-full h-auto pr-8 lg:hidden md:inline-block md:w-full'
               priority
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
             ></Image>
           </div>
-          <div className='w-1/2 flex flex-col items-center self-center'>
+          <div className='w-1/2 flex flex-col items-center self-center
+          lg:w-full lg:text-center'>
             <AnimatedText
               text='Helping provide solutions through Code and Design.'
               className='!text-5xl !text-left
@@ -31,7 +32,7 @@ export default function Home () {
             <p className='my-4 text-base'>
             As a highly skilled Full-Stack Developer, I am committed to staying at the forefront of technology trends and constantly enhancing my skill set. With a keen eye for innovation, I am actively seeking new opportunities to leverage my versatile skills and add value to a dynamic team. If you are in need of a dependable and personable Full-Stack Developer who consistently delivers exceptional results, look no further. Let's collaborate to create cutting-edge web applications that propel your business to new heights of success.
             </p>
-            <div className='flex items-center self-start mt-2'>
+            <div className='flex items-center self-start mt-2 lg:self-center'>
               <Link href='https://resume.creddle.io/resume/8x8kylcpyj1' target='_blank'
               className="flex item-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
 
@@ -50,7 +51,7 @@ export default function Home () {
         </div>
       </Layout>
       <HireMe/>
-      <div className='absolute right-8 bottom-8 inline=block w-24'>
+      <div className='absolute right-8 bottom-8 inline=block w-24 md:hidden'>
         <Image src={lightBulb} alt='CodeIt.Wiki' className='w-full h-auto'/>
       </div>
     </main>
