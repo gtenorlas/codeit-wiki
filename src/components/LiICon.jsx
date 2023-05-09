0403ae8c2000666160657d80432ea3c64368110c
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-09 09:27:06 -0400

    implement dark mode in LiIcon.jsx

--- src/components/LiICon.jsx
+++ src/components/LiICon.jsx
@@ -7,19 +7,19 @@ const LiICon = ({ reference }) => {
     offset: ["center end", "center center"],
   });
   return (
-    <figure className="absolute left-0 stroke-dark">
+    <figure className="absolute left-0 stroke-dark dark:stroke-light">
       <svg className="-rotate-90" width="75" height="75" viewBox="0 0 100 100">
         <circle
           cx="75"
           cy="50"
           r="20"
-          className="stroke-primary stroke-1 fill-none"
+          className="stroke-primary stroke-1 fill-none dark:stroke-primaryDark"
         />
         <motion.circle
           cx="75"
           cy="50"
           r="20"
-          className="fill-light stroke-[5px]"
+          className="fill-light stroke-[5px] dark:fill-dark"
           style={{
             pathLength: scrollYProgress,
           }}
@@ -28,7 +28,7 @@ const LiICon = ({ reference }) => {
           cx="75"
           cy="50"
           r="10"
-          className="fill-primary stroke-1 animate-pulse"
+          className="fill-primary stroke-1 animate-pulse dark:fill-primaryDark"
         />
       </svg>
     </figure>
