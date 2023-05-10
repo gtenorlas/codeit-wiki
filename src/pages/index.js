637089266fb71f8851eb313e6fbc6eeae8aff284
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-09 23:29:15 -0400

    adjust font-sizes in index.js per screen sizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 export default function Home () {
   return (
     <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-      <Layout className='pt-0 md:pt-8 sm:pt-8'>
+      <Layout className='pt-0 md:pt-4 sm:pt-0'>
         <div className='flex items-center justify-between w-full lg:flex-col'>
           <div className='w-1/2 pr-8 md:w-full md:pr-0 md:h-auto'>
             <Image
@@ -29,20 +29,22 @@ export default function Home () {
               xl:!text-4xl lg:!text-center lg:!text-5xl md:!text-4xl sm:!text-2xl
               '
             />
-            <p className='my-4 text-base'>
+            <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
             As a highly skilled Full-Stack Developer, I am committed to staying at the forefront of technology trends and constantly enhancing my skill set. With a keen eye for innovation, I am actively seeking new opportunities to leverage my versatile skills and add value to a dynamic team. If you are in need of a dependable and personable Full-Stack Developer who consistently delivers exceptional results, look no further. Let's collaborate to create cutting-edge web applications that propel your business to new heights of success.
             </p>
             <div className='flex items-center self-start mt-2 lg:self-center'>
               <Link href='https://resume.creddle.io/resume/8x8kylcpyj1' target='_blank'
               className="flex item-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
-
               dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
+              md:p-2 md:px-4 md:text-base
               "
               >
                 Resume <LinkArrow className="w-6 ml-1"/>
               </Link>
               <Link href='mailto:gene.tenorlas@yahoo.com' target='_blank'
-              className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light"
+              className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light
+              md:text-base
+              "
               >
                 Contact Me
               </Link>
