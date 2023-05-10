c43da2bdd9c890ccc252d29971145ec82d1c1024
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-10 15:10:12 -0400

    make the Skills.jsx component responsive

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -23,6 +23,8 @@ const Skills = () => {
       <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark
       lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]
       lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+      md:bg-circularLightMd md:dark:bg-circularDarkMd
+      sm:bg-circularLightSm sm:dark:bg-circularDarkSm
       ">
         <motion.div
           className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer  dark:bg-light dark:text-dark
