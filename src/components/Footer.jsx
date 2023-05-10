4b53e3c8f6fe33d33f18efc37a776f983b5d0bdb
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-10 16:56:02 -0400

    make the Footer.jsx responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
     <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light
     sm:text-base">
       <Layout className="py-8 flex flex-center justify-between lg:flex-col lg:py-6 lg:items-center">
-        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-        <div className="flex items-center lg:py-2">Developed by<span>&nbsp;Gene Tenorlas&nbsp;</span>from CodeIt.Wiki</div>
+        <span className="sm:text-base sm:text-center">{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+        <div className="flex items-center lg:py-2 xs:text-sm xs:flex-col">Developed by<span>&nbsp;Gene Tenorlas&nbsp;</span>from CodeIt.Wiki</div>
         <Link href="mailto:gene.tenorlas@yahoo.com" target="_blank" className="underline underline-offset-2">Contact Me</Link>
       </Layout>
     </footer>
