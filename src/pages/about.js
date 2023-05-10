c5e4ae8499905692d0d34ea115bfabae5e76ccca
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-10 14:33:48 -0400

    make about me in about.js responsive

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,10 +41,11 @@ const about = () => {
         <Layout className='pt-16'>
           <AnimatedText
             text='Solving Ignites My Creativity'
-            className='mb-10'
+            className='mb-10 lg:!text-6xl sm:!text-5xl xs:!text-3xl sm:mb-8'
           />
-          <div className='grid w-full grid-cols-8 gap-16'>
-            <section className='col-span-3 flex flex-col items-start justify-start'>
+          <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+            <section className='col-span-3 flex flex-col items-start justify-start
+            xl:col-span-4 md:order-2 md:col-span-8'>
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>
                 About Me
               </h2>
@@ -74,7 +75,9 @@ const about = () => {
                 together to create something truly amazing!
               </p>
             </section>
-            <section className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+            <section className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light
+            xl:col-span-4 md:order-1 md:col-span-8
+            '>
               <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
               <Image
                 src={profilePicture}
@@ -84,28 +87,28 @@ const about = () => {
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               ></Image>
             </section>
-            <div className='col-span-2 flex flex-col items-end justify-between'>
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inline-block text-7xl font-bold'>
+            <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center  md:order-3'>
+              <div className='flex flex-col items-end justify-center xl:items-center'>
+                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                   <AnimatedNumbers value={20} />+
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
                   projects completed
                 </h2>
               </div>
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inline-block text-7xl font-bold'>
+              <div className='flex flex-col items-end justify-center xl:items-center'>
+                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                   <AnimatedNumbers value={30} />+
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
                   blogs/videos completed
                 </h2>
               </div>
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inline-block text-7xl font-bold'>
+              <div className='flex flex-col items-end justify-center xl:items-center'>
+                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                   <AnimatedNumbers value={6} />+
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
                   years of experience
                 </h2>
               </div>
