e20db0211851cb0f84eac16287473d2175f20d54
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-05-10 16:42:35 -0400

    make the blogs.js responsive

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -43,7 +43,7 @@ const MovingImg = ({ title, img, link }) => {
         ref={imgRef}
         src={img}
         alt={title}
-        className='z-10 w-96 auto hidden absolute rounded-lg'
+        className='z-10 w-96 auto hidden absolute rounded-lg md:!hidden'
       />
     </Link>
   )
@@ -57,10 +57,11 @@ const Blog = ({ img, title, date, link }) => {
       viewport={{ once: true }}
       className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark
     border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light
+    sm:flex-col
     '
     >
       <MovingImg title={title} img={img} link={link} />
-      <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>
+      <span className='text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm'>
         {date}
       </span>
     </motion.li>
@@ -87,7 +88,8 @@ const FeaturedBlog = ({ img, title, time, summary, link }) => {
         />
       </Link>
       <Link href={link} targt='_blank'>
-        <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline hover:underline-offset-2'>
+        <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline hover:underline-offset-2
+        xs:text-lg'>
           {title}
         </h2>
       </Link>
@@ -109,8 +111,8 @@ const blogs = () => {
       </Head>
       <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
         <Layout className='pt-16'>
-          <AnimatedText text='Stay up-to-date with Blogs' className='mb-16' />
-          <ul className='grid grid-cols-2 gap-16'>
+          <AnimatedText text='Stay up-to-date with Blogs' className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl' />
+          <ul className='grid grid-cols-2 gap-16 md:grid-cols-1 lg:gap-8 md:gap-y-16'>
             <FeaturedBlog
               title='ORMs for JavaScript'
               summary='Explain what ORM is and provide known ORMs in Javascript.'
